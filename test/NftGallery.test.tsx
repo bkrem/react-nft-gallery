97976bc47d4b77037b20291474a29d2917f5797f
Ben Kremer <contact@bkrem.dev>
2021-07-27 18:35:16 +0700

    chore: rename test file

--- test/NftGallery.test.tsx
+++ test/NftGallery.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { NftGallery } from '../src/NftGallery';
 
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<NftGallery ownerAddress="" />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
