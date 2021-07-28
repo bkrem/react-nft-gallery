1a6e94fcd1de51ec04e1ef03f1264af5292f5600
Ben Kremer <contact@bkrem.dev>
2021-07-29 01:38:53 +0700

    fix: simplify arrangement

--- src/NftGallery.tsx
+++ src/NftGallery.tsx
@@ -9,34 +9,30 @@ const Grid = styled.div`
   margin-top: 3rem;
 `;
 
-const CardWrapper = styled.div`
-  height: calc(50vw - 26.5px);
-  margin: 7.5px;
-  width: calc(50vw - 26.5px);
-
+const AssetWrapper = styled.article`
   @media (max-width: 1500px) {
-    height: calc(((100vw - 403px) - 90px) / 4);
+    /* height: calc(((100vw - 403px) - 90px) / 4); */
     width: calc(((100vw - 403px) - 90px) / 4);
   }
   @media (max-width: 1300px) {
-    height: calc(((100vw - 403px) - 60px) / 3);
+    /* height: calc(((100vw - 403px) - 60px) / 3); */
     width: calc(((100vw - 403px) - 60px) / 3);
   }
   @media (max-width: 1000px) {
-    height: calc(((100vw - 403px) - 30px) / 2);
+    /* height: calc(((100vw - 403px) - 30px) / 2); */
     width: calc(((100vw - 403px) - 30px) / 2);
   }
   @media (max-width: 850px) {
     margin: 9.5px;
-    height: calc((100vw - 86px) / 3);
+    /* height: calc((100vw - 86px) / 3); */
     width: calc((100vw - 86px) / 3);
   }
   @media (max-width: 650px) {
-    height: calc(50vw - 33.5px);
+    /* height: calc(50vw - 33.5px); */
     width: calc(50vw - 33.5px);
   }
   @media (max-width: 480px) {
-    height: calc(50vw - 26.5px);
+    /* height: calc(50vw - 26.5px); */
     margin: 7.5px;
     width: calc(50vw - 26.5px);
   }
@@ -55,7 +51,9 @@ export interface NftGalleryProps {
 
 const OPENSEA_API_OFFSET = 50;
 
-export const NftGallery: React.FC<NftGalleryProps> = ({ ownerAddress }) => {
+export const NftGallery: React.FC<NftGalleryProps> = ({
+  ownerAddress = '',
+}) => {
   const [assets, setAssets] = useState([] as any[]);
   const [currentOffset, setCurrentOffset] = useState(0);
   const [canLoadMore, setCanLoadMore] = useState(false);
@@ -77,7 +75,7 @@ export const NftGallery: React.FC<NftGalleryProps> = ({ ownerAddress }) => {
     const displayableAssets = rawAssets.filter((asset: any) =>
       Boolean(asset.image_preview_url)
     );
-    console.log('Got %s assets', rawAssets.length);
+    console.log('Got %s assets', rawAssets.length, rawAssets);
     console.log('%s displayable assets', displayableAssets.length);
 
     setAssets((prevAssets) => [...prevAssets, ...displayableAssets]);
@@ -93,17 +91,22 @@ export const NftGallery: React.FC<NftGalleryProps> = ({ ownerAddress }) => {
       <Grid>
         {/* TODO: Handle rendering of .mp4 previews */}
         {assets.map((asset: any) => (
-          <CardWrapper key={asset.id}>
-            {/* <a key={asset.name + i} href="#" style={styles.card as any}> */}
-            {/* <h2>{asset.name}</h2> */}
-            {/* <p>{asset.description}</p> */}
-            <Img
-              src={asset.image_preview_url}
-              alt={asset.name}
-              loading="lazy"
-            />
-            {/* </a> */}
-          </CardWrapper>
+          <AssetWrapper
+            style={{ backgroundColor: 'papayawhip' }}
+            key={asset.id}
+          >
+            <div>
+              <Img
+                src={asset.image_preview_url}
+                alt={asset.name}
+                loading="lazy"
+              />
+              <div>
+                <p>Name: {asset.name}</p>
+                <p>Collection: {asset.collection.name}</p>
+              </div>
+            </div>
+          </AssetWrapper>
         ))}
       </Grid>
       {canLoadMore && (
