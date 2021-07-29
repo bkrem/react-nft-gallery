c74de64a5f4f9c1e68937b64e4717fc8c9979933
Ben Kremer <contact@bkrem.dev>
2021-07-29 18:21:55 +0700

    fix(ci): tries to make unit tests play nice

--- test/NftGallery.test.tsx
+++ test/NftGallery.test.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
 import { NftGallery } from '../src/NftGallery';
+
+// @ts-ignore
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ assets: [] }),
+  })
+);
 
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<NftGallery ownerAddress="" />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    act(() => {
+      ReactDOM.render(<NftGallery ownerAddress="" />, div);
+    });
+    // ReactDOM.unmountComponentAtNode(div);
   });
 });
