2880001f65b197cf65f0ad3bd503a23a254969b8
Ben Kremer <contact@bkrem.dev>
2021-07-31 00:12:11 +0700

    chore(deps): fix build for example sub-folder

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { NftGallery } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <NftGallery ownerAddress="" />
     </div>
   );
 };
