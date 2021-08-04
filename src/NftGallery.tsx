251333c39a835e2204f51a67b8c8543f24ec5525
Ben Kremer <contact@bkrem.dev>
2021-08-04 17:52:17 +0700

    feat: implements metadata section

--- src/NftGallery.tsx
+++ src/NftGallery.tsx
@@ -1,32 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 
-import './index.css';
+import './tailwind.css';
 
-const DescriptionOverlay = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 1rem;
-  color: white;
-  text-align: center;
-  background-color: transparent;
-  opacity: 0;
+// const DescriptionOverlay = styled.div`
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 100%;
+//   border-radius: 1rem;
+//   color: white;
+//   text-align: center;
+//   background-color: transparent;
+//   opacity: 0;
 
-  &:hover {
-    cursor: pointer;
-    background-color: rgba(0, 0, 0, 0.8);
-    opacity: 1;
-  }
-`;
+//   &:hover {
+//     cursor: pointer;
+//     background-color: rgba(0, 0, 0, 0.8);
+//     opacity: 1;
+//   }
+// `;
 
-const Description = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
+// const Description = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+// `;
 
 const Img = styled.img`
   width: 100%;
@@ -35,32 +35,65 @@ const Img = styled.img`
   border-radius: 1rem;
 `;
 
-export interface NftGalleryProps {
-  /** Ethereum address for which the gallery should contain associated NFTs. */
-  ownerAddress: string;
-}
-
 const OPENSEA_API_OFFSET = 50;
 
-const ImageComponent: React.FC<{ photo: { asset: any; src: any } }> = ({
-  photo,
-}) => {
-  const { src, asset } = photo;
+// const ImageWithOverlay: React.FC<{ asset: any }> = ({ asset }) => {
+//   return (
+//     <article className="relative rounded-2xl">
+//       <Img src={asset.image_preview_url} alt={asset.name} loading="lazy" />
+//       <DescriptionOverlay>
+//         <Description>
+//           <h1>{asset.name}</h1>
+//           <h2>Collection: {asset.collection.name}</h2>
+//         </Description>
+//       </DescriptionOverlay>
+//     </article>
+//   );
+// };
+
+const ImageWithAttributes: React.FC<{ asset: any }> = ({ asset }) => {
   return (
-    <article className="relative rounded-2xl" key={asset.id}>
-      <Img src={src} alt={asset.name} loading="lazy" />
-      <DescriptionOverlay>
-        <Description>
-          <h1>{asset.name}</h1>
-          <h2>Collection: {asset.collection.name}</h2>
-        </Description>
-      </DescriptionOverlay>
+    <article className="rounded-2xl bg-white dark:bg-gray-800">
+      <div style={{ height: '20rem' }}>
+        <Img src={asset.image_preview_url} alt={asset.name} loading="lazy" />
+      </div>
+      <div className="p-4">
+        <div className="break-words cursor-pointer truncate text-lg font-semibold dark:text-gray-200">
+          {asset.name}
+        </div>
+        <hr className="mx-4 border-gray-100 dark:border-gray-800" />
+        <div className="flex items-center">
+          {asset.collection.image_url && (
+            <img
+              src={asset.collection.image_url}
+              alt={asset.collection.name}
+              className="rounded-full mr-2 w-8 h-8"
+            />
+          )}
+          <div className="text-sm font-semibold truncate dark:text-gray-200">
+            {asset.collection.name}
+          </div>
+        </div>
+      </div>
     </article>
   );
 };
 
+// TODO: add `metadataIsVisible`
+export interface NftGalleryProps {
+  /**
+   * Ethereum address for which the gallery should contain associated NFTs.
+   */
+  ownerAddress: string;
+  /**
+   * Sets whether the gallery is displayed in dark mode.
+   */
+  darkMode?: boolean;
+}
+
 export const NftGallery: React.FC<NftGalleryProps> = ({
   ownerAddress = '',
+  darkMode = true,
 }) => {
   const [assets, setAssets] = useState([] as any[]);
   const [currentOffset, setCurrentOffset] = useState(0);
@@ -94,32 +127,32 @@ export const NftGallery: React.FC<NftGalleryProps> = ({
     loadAssets(ownerAddress, currentOffset);
   }, [ownerAddress, currentOffset]);
 
-  /* TODO: Handle rendering of .mp4 previews */
+  // TODO: Handle rendering of .mp4 previews
+  // TODO: handle rendering of non-image assets (e.g. ENS,...)
   return (
-    // TODO: remove debug bg
-    <div className="bg-yellow-100">
-      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {assets.map((asset: any) => (
-          <ImageComponent
-            photo={{
-              asset,
-              src: asset.image_preview_url,
-            }}
-          />
-        ))}
-      </div>
-      {canLoadMore && (
-        <div className="flex justify-center">
-          <button
-            className="p-4"
-            onClick={() => {
-              setCurrentOffset((prevOffset) => prevOffset + OPENSEA_API_OFFSET);
-            }}
-          >
-            Load more
-          </button>
+    <section className={darkMode ? 'dark' : ''}>
+      {/* TODO: remove debug bg color */}
+      <div className="bg-yellow-100 dark:bg-gray-900">
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {assets.map((asset: any) => (
+            <ImageWithAttributes key={asset.id} asset={asset} />
+          ))}
         </div>
-      )}
-    </div>
+        {canLoadMore && (
+          <div className="flex justify-center">
+            <button
+              className="p-4"
+              onClick={() => {
+                setCurrentOffset(
+                  (prevOffset) => prevOffset + OPENSEA_API_OFFSET
+                );
+              }}
+            >
+              Load more
+            </button>
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./src/**/*.tsx'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
