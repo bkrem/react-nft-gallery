5dd8c85474e321554678e25b6b606115e429ab96
Ben Kremer <contact@bkrem.dev>
2021-08-05 19:43:18 +0700

    feat: adds handling for .mp4 assets

--- src/components/GalleryItem.tsx
+++ src/components/GalleryItem.tsx
@@ -10,31 +10,57 @@ export const GalleryItem: React.FC<GalleryItemProps> = ({
   asset,
   metadataIsVisible,
 }) => {
+  const assetTitle = asset.name || asset.token_id;
+
+  const renderAssetMedia = () => {
+    // No media present -> render the name/tokenId as a placeholder.
+    if (!asset.image_preview_url) {
+      return (
+        <div className="flex flex-col justify-center items-center w-full h-full break-words cursor-pointer truncate text-lg font-semibold dark:text-gray-200">
+          {assetTitle}
+        </div>
+      );
+    }
+
+    const assetMediaExt = asset.image_preview_url.split('.').pop();
+
+    if (assetMediaExt === 'mp4') {
+      return (
+        <video
+          style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+          src={asset.image_preview_url}
+          preload="auto"
+          controlsList="nodownload"
+          autoPlay
+          loop
+          playsInline
+        ></video>
+      );
+    }
+
+    return (
+      <img
+        className={`w-full h-full object-cover ${
+          metadataIsVisible ? 'rounded-t-2xl' : 'rounded-2xl'
+        }`}
+        src={asset.image_preview_url}
+        alt={asset.name}
+        loading="lazy"
+      />
+    );
+  };
+
   return (
     <article className="rounded-2xl bg-white dark:bg-gray-800">
-      <div>
-        {asset.image_preview_url ? (
-          <img
-            className={`w-full h-full object-cover ${
-              metadataIsVisible ? 'rounded-t-2xl' : 'rounded-2xl'
-            }`}
-            src={asset.image_preview_url}
-            alt={asset.name}
-            loading="lazy"
-          />
-        ) : (
-          <div className="flex flex-col justify-center items-center w-full h-full break-words cursor-pointer truncate text-lg font-semibold dark:text-gray-200">
-            {asset.name || asset.token_id}
-          </div>
-        )}
-      </div>
+      <div style={{ height: '20rem' }}>{renderAssetMedia()}</div>
       {metadataIsVisible && (
         <div data-test-id="metadata-section" className="p-4">
           <div className="break-words cursor-pointer truncate text-lg font-semibold dark:text-gray-200">
-            {asset.name}
+            {assetTitle}
           </div>
           <hr className="mx-2 my-4 border-gray-100 dark:border-gray-900" />
           <div className="flex items-center">
+            y
             {asset.collection.image_url && (
               <img
                 src={asset.collection.image_url}
