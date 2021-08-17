9654e16e173ee21de0bec2c22ca05ce22f82b212
Ben Kremer <contact@bkrem.dev>
2021-08-17 22:48:52 +0200

    fix(storybook): tweak naming of default story

--- stories/NftGallery.stories.tsx
+++ stories/NftGallery.stories.tsx
@@ -18,8 +18,8 @@ const Template: Story<NftGalleryProps> = (args) => <NftGallery {...args} />;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
-export const Default = Template.bind({});
-Default.args = {};
+export const DefaultWithLatestMints = Template.bind({});
+DefaultWithLatestMints.args = {};
 
 export const DarkMode = Template.bind({});
 DarkMode.args = { darkMode: true } as NftGalleryProps;
